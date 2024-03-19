731cd32d6ba4ae2dfc6ba86f0dbe5f395b4ca90c
Jonathan Collinet <john.collinet@gmail.com>
2024-03-19 22:55:19 +0100

    fix: Fix Help page link size
    the link sizes are small

--- src/components/Help/Summary.vue
+++ src/components/Help/Summary.vue
@@ -8,8 +8,8 @@ defineProps<{
 
 <template>
     <div class="flex flex-col items-start border-l-2 pl-4 sm:flex-row sm:space-x-4">
-        <UiLink v-for="(chapter, index) in chapters" :key="index" :to="`${Urls.Help}/${chapter.name}`">{{
-            $t("pages.help.summary." + chapter.name)
-        }}</UiLink>
+        <UiLink v-for="(chapter, index) in chapters" :key="index" class="text-sm" :to="`${Urls.Help}/${chapter.name}`">
+            {{ $t("pages.help.summary." + chapter.name) }}
+        </UiLink>
     </div>
 </template>
