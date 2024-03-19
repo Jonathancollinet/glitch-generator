df83a7a0bf9cf3c630ecbb8d0837ece3bbe79186
Jonathan Collinet <john.collinet@gmail.com>
2024-03-19 16:28:49 +0100

    feat: Remove version

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,10 +1,7 @@
-<script type="ts" setup>
-import { version } from "~/utils/constants";
-</script>
 <template>
     <div class="flex justify-center pb-6 pt-12">
         <UiText data-size="xs">
-            {{ $t("footer.text") }} {{ version }} -
+            {{ $t("footer.text") }} -
             {{ $t("footer.inspiration") }}
         </UiText>
         <UiText data-size="xs">
